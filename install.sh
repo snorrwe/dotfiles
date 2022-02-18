417bbf46c98ba4ab857938366771c8bf64d62251
Daniel Kiss <littlesnorrboy@gmail.com>
2022-02-18 13:12:36 +0100

    Add more install stuff

--- install.sh
+++ install.sh
@@ -7,7 +7,22 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
 . "$HOME/.cargo/env"
 
 # install rust fluff
-cargo install ripgrep zoxide alacritty
+cargo install ripgrep zoxide alacritty fd-find
 
 # install starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+# install brew
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/bin/nvim
+# clone nvim config
+git clone https://github.com/snorrwe/nvim-config $HOME/.config/nvim
+
+#install stuff via brew
+brew install fzf
+$(brew --prefix)/opt/fzf/install
+brew install lazygit
