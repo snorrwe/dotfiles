926bea52b9f51ef9f16b4d1f502210f1278636e9
Daniel Kiss <littlesnorrboy@gmail.com>
2022-02-18 13:32:14 +0100

    Add rust-analyzer install

--- install.sh
+++ install.sh
@@ -12,6 +12,10 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
 
 # install rust fluff
 cargo install ripgrep zoxide alacritty fd-find
+# install rust-analyzer
+mkdir -p ~/.local/bin
+curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
+chmod +x ~/.local/bin/rust-analyzer
 
 # install starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
