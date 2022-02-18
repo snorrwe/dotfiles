ab017c1f4176baf562e244867a19e46653959b1c
Daniel Kiss <littlesnorrboy@gmail.com>
2022-02-18 18:10:49 +0100

    fmt

--- install.sh
+++ install.sh
@@ -21,7 +21,27 @@ function install_apt_stuff {
 # install llvm
 (
     set -e
-    apt-get install -y clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang
+    apt-get install -y \
+        clang-format \
+        clang-tidy \
+        clang-tools \
+        clang \
+        clangd \
+        libc++-dev \
+        libc++1 \
+        libc++abi-dev \
+        libc++abi1 \
+        libclang-dev \
+        libclang1 \
+        liblldb-dev \
+        libomp-dev \
+        libomp5 \
+        lld \
+        lldb \
+        llvm-dev \
+        llvm-runtime \
+        llvm \
+        python3-clang
 )
 
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
