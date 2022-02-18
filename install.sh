1f3c214c0e4ff7dc4d65ad98a71533f0c5b6427c
Daniel Kiss <littlesnorrboy@gmail.com>
2022-02-18 18:08:46 +0100

    Attempt to fix llvm install

--- install.sh
+++ install.sh
@@ -18,8 +18,12 @@ function install_apt_stuff {
     set -e
     install_apt_stuff
 )
-
 # install llvm
+(
+    set -e
+    apt-get install -y clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang
+)
+
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
 # install brew
