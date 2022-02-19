917ee00b0d578861eb50487d7c2d3096966836c5
Daniel Kiss <littlesnorrboy@gmail.com>
2022-02-19 18:58:34 +0100

    Fix docker setup

--- _setup/setup_docker.sh
+++ _setup/setup_docker.sh
@@ -6,5 +6,6 @@ brew install docker
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
-newgrp docker 
-docker run hello-world
+
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
