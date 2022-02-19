c42d5fdb3829cd5731dbc362260f2062a4ca1945
Daniel Kiss <littlesnorrboy@gmail.com>
2022-02-19 19:35:12 +0100

    Allow docker group setup to fail

--- _setup/setup_docker.sh
+++ _setup/setup_docker.sh
@@ -4,7 +4,10 @@ set -ex
 
 sudo apt install -y docker.io
 
+(
 sudo groupadd docker
 sudo usermod -aG docker $USER
+) || echo "Failed groupadd"
 
 sudo systemctl start docker
+sudo systemctl enable docker
