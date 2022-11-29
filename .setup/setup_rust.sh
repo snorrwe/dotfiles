9ae8d4b794660a71d1c3497be864e5bc78b8e8b9
Daniel Kiss <littlesnorrboy@gmail.com>
2022-11-29 17:56:30 +0100

    Do not install scache on setup

--- .setup/setup_rust.sh
+++ .setup/setup_rust.sh
@@ -13,7 +13,5 @@ mkdir -p ~/.local/bin
 curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
 chmod +x ~/.local/bin/rust-analyzer
 
-cargo install -f sccache --no-default-features
-export RUSTC_WRAPPER=$HOME/.cargo/bin/sccache
 # install rust fluff
 cargo install -f ripgrep zoxide fd-find
