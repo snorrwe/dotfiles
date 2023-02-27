82d45d4a34c3b79f27bde59c31b04e4d8d7ba696
Daniel Kiss <littlesnorrboy@gmail.com>
2023-02-27 11:49:44 +0100

    Always put the tray on the primary monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,7 +4,13 @@
 # Terminate already running bad instances
 killall -q polybar
 
+PRIMARY=$(polybar --list-monitors | grep "primary" | cut -d":" -f1)
+OTHERS=$(polybar --list-monitors | grep -v "primary" | cut -d":" -f1)
+
+MONITOR=$PRIMARY TRAY=right polybar --reload main_bar &
+sleep 1
+
 # Launch the bar on all monitors
-for m in $(polybar --list-monitors | cut -d":" -f1); do
+for m in $OTHERS; do
     MONITOR=$m polybar --reload main_bar &
 done
