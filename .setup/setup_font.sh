0a51afc5823985709487853255a6e3401ff788c2
Daniel Kiss <littlesnorrboy@gmail.com>
2023-06-12 20:56:50 +0200

    Use the intel-one-mono font

--- .setup/setup_font.sh
+++ .setup/setup_font.sh
@@ -8,3 +8,9 @@ curl -fLo "CaskaydiaCove.zip" \
     https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip
 
 unzip CaskaydiaCove.zip
+
+curl -fLo "intelone-mono.zip" \
+    https://github.com/intel/intel-one-mono/releases/download/V1.2.1/ttf.zip
+
+unzip intelone-mono.zip
+mv ttf/* ./
