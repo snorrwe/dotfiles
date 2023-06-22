19989d932022b90dfa049c81940c0b783a731881
Daniel Kiss <littlesnorrboy@gmail.com>
2023-06-22 22:02:13 +0200

    Tweak

--- .setup/setup_font.sh
+++ .setup/setup_font.sh
@@ -3,7 +3,7 @@
 set -ex
 
 mkdir -p $HOME/.local/share/fonts
-cd $HOME/.local/share/fonts && 
+cd $HOME/.local/share/fonts
 curl -fLo "CaskaydiaCove.zip" \
     https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip
 
