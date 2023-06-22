b4751f076a4a152b660887f2277ed0ff5a956a10
Daniel Kiss <littlesnorrboy@gmail.com>
2023-06-22 20:45:58 +0200

    Add setup_yay

--- .setup/setup_flatpak.sh
+++ .setup/setup_flatpak.sh
@@ -2,11 +2,10 @@
 
 set -ex
 
-flatpak install -y \
-    com.bitwarden.desktop  \
-    com.discordapp.Discord \
-    com.slack.Slack \
-    md.obsidian.Obsidian \
-    org.mozilla.Thunderbird \ 
-    org.telegram.desktop \
-    com.spotify.Client
+flatpak install -y com.bitwarden.desktop
+flatpak install -y com.discordapp.Discord
+flatpak install -y com.slack.Slack
+flatpak install -y md.obsidian.Obsidian
+flatpak install -y org.mozilla.Thunderbird
+flatpak install -y org.telegram.desktop
+flatpak install -y com.spotify.Client
---
+++ .setup/setup_yay.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+
+set -ex
+
+sudo pacman -Sq --noconfirm \
+    git \
+    base-devel
+
+cd $(mktemp -d)
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
