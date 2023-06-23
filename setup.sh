aaa0b2d28dea50034f1e5737d15153c6eac4ef47
Daniel Kiss <littlesnorrboy@gmail.com>
2023-06-23 20:39:39 +0200

    Clone tpm on setup

--- setup.sh
+++ setup.sh
@@ -36,14 +36,14 @@ function install_apt_stuff {
 
 mkdir -p dev
 
+( bash .setup/setup_yay.sh )
 ( bash .setup/setup_nvim.sh )
-
 ( bash .setup/setup_rust.sh )
-
 ( bash .setup/setup_font.sh )
 ( bash .setup/setup_docker.sh )
 ( bash .setup/setup_flatpak.sh )
 ( bash .setup/setup_i3.sh )
-( bash .setup/setup_yay.sh )
 ( bash .setup/setup_zsh.sh )
 
+# tmux setup
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
