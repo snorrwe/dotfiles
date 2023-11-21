764835b3eedabe0f5da7292e4fa024d33f518ff4
Daniel Kiss <littlesnorrboy@gmail.com>
2023-11-21 23:56:54 +0100

    Download intel fonts into tmp/ instead of manual cleanup

--- .setup/setup_font.sh
+++ .setup/setup_font.sh
@@ -6,19 +6,12 @@ mkdir -p "$HOME"/.local/share/fonts
 cd "$HOME"/.local/share/fonts
 curl -fLo "CaskaydiaCove.zip" \
     https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip
-
 unzip CaskaydiaCove.zip
 
+(
+cd "$(mktemp -d)"
 curl -fLo "intelone-mono.zip" \
     https://github.com/intel/intel-one-mono/releases/download/V1.2.1/ttf.zip
-
 unzip intelone-mono.zip
-mv ttf/* ./
-rm -rf ttf
-rm -f intelone-mono.zip
-
-curl -fLo "ComicMono.ttf" \
-    https://dtinth.github.io/comic-mono-font/ComicMono.ttf
-curl -fLo "ComicMono-Bold.ttf" \
-    https://dtinth.github.io/comic-mono-font/ComicMono-Bold.ttf
-
+mv ttf/* "$HOME/.local/share/fonts/"
+)
