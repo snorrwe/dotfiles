b90d2a2f4168985353dd70dc3b63de759d0e4b13
Daniel Kiss <littlesnorrboy@gmail.com>
2024-01-09 15:57:25 +0100

    Fix warnings

--- .setup/setup_nvim.sh
+++ .setup/setup_nvim.sh
@@ -6,9 +6,9 @@ set -ex
 sudo yay -Syu --noconfirm fuse2 neovim-git
 
 # clone nvim config
-if [ -d $HOME/.config/nvim ];
+if [ -d "$HOME"/.config/nvim ];
 then
-    rm -rf $HOME/.config/nvim
+    rm -rf "$HOME"/.config/nvim
 fi
-git clone https://github.com/snorrwe/nvim-config $HOME/.config/nvim
-python3 -m venv $HOME/.config/nvim/python3
+git clone https://github.com/snorrwe/nvim-config "$HOME"/.config/nvim
+python3 -m venv "$HOME"/.config/nvim/python3
