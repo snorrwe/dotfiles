3e34fbdfda3ad1c1c22498a30300a1884e15d78b
Daniel Kiss <littlesnorrboy@gmail.com>
2024-05-16 10:59:43 +0200

    Fix sub-setup paths

--- .setup/setup.sh
+++ .setup/setup.sh
@@ -44,10 +44,10 @@ if [ ! -d "$HOME"/.tmux/plugins/tpm ]; then
     git clone https://github.com/tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm
 fi
 
-( bash ./.setup/setup_yay.sh )
-( bash ./.setup/setup_nvim.sh )
-( bash ./.setup/setup_rust.sh )
-( bash ./.setup/setup_font.sh )
-( bash ./.setup/setup_docker.sh )
-( bash ./.setup/setup_i3.sh )
-( bash ./.setup/setup_zsh.sh )
+( bash ./setup_yay.sh )
+( bash ./setup_nvim.sh )
+( bash ./setup_rust.sh )
+( bash ./setup_font.sh )
+( bash ./setup_docker.sh )
+( bash ./setup_i3.sh )
+( bash ./setup_zsh.sh )
