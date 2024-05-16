67262101e46acda6f5906dad301096727a4a8731
Daniel Kiss <littlesnorrboy@gmail.com>
2024-05-16 11:21:12 +0200

    Fix setup_zsh

--- .setup/setup_zsh.sh
+++ .setup/setup_zsh.sh
@@ -4,4 +4,4 @@ set -ex
 
 # install znap
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-snap.git "$HOME"/zsh-snap
-source "$HOME"/zsh-snap/znap.zsh
+zsh -c "source $HOME/zsh-snap/znap.zsh"
