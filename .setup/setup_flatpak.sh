0b845ed694c1e68016c5a759734aae8b8d6722ce
Daniel Kiss <littlesnorrboy@gmail.com>
2024-06-15 19:59:01 +0000

    Add slack

--- .setup/setup_flatpak.sh
+++ .setup/setup_flatpak.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 sudo pacman -S --noconfirm extra/flatpak extra/flatpak-xdg-utils
 
-flatpak install --noninteractive us.zoom.Zoom com.spotify.Client
+flatpak install --noninteractive us.zoom.Zoom com.spotify.Client com.slack.Slack
