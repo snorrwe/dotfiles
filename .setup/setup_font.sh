e572508e13dd408ef342da3cf647c25a20f81669
Daniel Kiss <littlesnorrboy@gmail.com>
2024-07-14 23:53:27 +0200

    Remove obsolete setups

--- .setup/setup_font.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-set -ex
-
-mkdir -p "$HOME"/.local/share/fonts
-cd "$HOME"/.local/share/fonts
-curl -fLo "CaskaydiaCove.zip" \
-    https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip
-unzip CaskaydiaCove.zip
--- .setup/setup_i3.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-set -ex
-
-sudo pacman -Sq --noconfirm \
-    i3-wm \
-    polybar \
-    picom \
-    rofi
