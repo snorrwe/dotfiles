49a981dde9945e3dcd0ccbac40828e36b75d0b13
Daniel Kiss <littlesnorrboy@gmail.com>
2024-07-15 15:56:11 +0200

    Disable plasma

--- nix-stuff/configuration.nix
+++ nix-stuff/configuration.nix
@@ -33,7 +33,6 @@
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.wayland.enable = true;
   services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
