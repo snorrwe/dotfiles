87a37d92914d0806657aeb76d447750ac678b364
Daniel Kiss <littlesnorrboy@gmail.com>
2024-07-15 15:59:52 +0200

    Remove comment

--- nix-stuff/configuration.nix
+++ nix-stuff/configuration.nix
@@ -60,9 +60,6 @@
     #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.snorrwe = {
     isNormalUser = true;
