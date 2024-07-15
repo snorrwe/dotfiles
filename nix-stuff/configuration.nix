0617dc3922acbc684fac45ddad9c7b39cd457a8b
Daniel Kiss <littlesnorrboy@gmail.com>
2024-07-15 13:17:46 +0200

    Fix sddm

--- nix-stuff/configuration.nix
+++ nix-stuff/configuration.nix
@@ -31,6 +31,7 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.wayland.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
