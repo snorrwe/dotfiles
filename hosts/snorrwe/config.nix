f6668f9670408fcc8b887904b75ce58734a5e6f7
Daniel Kiss <littlesnorrboy@gmail.com>
2024-07-26 10:40:34 +0200

    Add vulkan support

--- hosts/snorrwe/config.nix
+++ hosts/snorrwe/config.nix
@@ -10,6 +10,7 @@
   imports = [
     ./hardware.nix
     ./sound.nix
+    ./video.nix
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -63,31 +64,6 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   services.displayManager.sddm.enable = true;
-
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "550.90.07";
-    sha256_64bit = "sha256-Uaz1edWpiE9XOh0/Ui5/r6XnhB4iqc7AtLvq4xsLlzM=";
-    openSha256 = "sha256-dktHCoESqoNfu5M73aY5MQGROlZawZwzBqs3RkOyfoQ=";
-    settingsSha256 = "sha256-qNjfsT9NGV151EHnG4fgBonVFSKc4yFEVomtXg9uYD4=";
-    persistencedSha256 = "sha256-ci86XGlno6DbHw6rkVSzBpopaapfJvk0+lHcR4LDq50=";
-
-    ibtSupport = true;
-  };
-  # Configure keymap in X11
-  services.xserver = {
-    xkb.layout = "us";
-    xkb.variant = "";
-    enable = true;
-    videoDrivers = [ "nvidia" ];
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dmenu
-        rofi
-        polybar
-      ];
-    };
-  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
