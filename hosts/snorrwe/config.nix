3c3ae4706fe8730519ea26ffc1cf03ad66c5dcd7
Daniel Kiss <littlesnorrboy@gmail.com>
2024-07-26 10:19:05 +0200

    Rollback nvidia driver version

--- hosts/snorrwe/config.nix
+++ hosts/snorrwe/config.nix
@@ -64,8 +64,16 @@
 
   services.displayManager.sddm.enable = true;
 
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "550.90.07";
+    sha256_64bit = "sha256-Uaz1edWpiE9XOh0/Ui5/r6XnhB4iqc7AtLvq4xsLlzM=";
+    openSha256 = "sha256-dktHCoESqoNfu5M73aY5MQGROlZawZwzBqs3RkOyfoQ=";
+    settingsSha256 = "sha256-qNjfsT9NGV151EHnG4fgBonVFSKc4yFEVomtXg9uYD4=";
+    persistencedSha256 = "sha256-ci86XGlno6DbHw6rkVSzBpopaapfJvk0+lHcR4LDq50=";
+
+    ibtSupport = true;
+  };
   # Configure keymap in X11
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
   services.xserver = {
     xkb.layout = "us";
     xkb.variant = "";
