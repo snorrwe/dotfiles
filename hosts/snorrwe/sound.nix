6c6dd1e3917a7c56fe5a8551cb86f2aa2865711d
Daniel Kiss <littlesnorrboy@gmail.com>
2024-07-26 11:28:47 +0200

    Fix usb headphones

--- hosts/snorrwe/sound.nix
+++ hosts/snorrwe/sound.nix
@@ -18,14 +18,10 @@
       support32Bit = true;
     };
     pulse.enable = true;
-
-  };
-  services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
-    "monitor.bluez.properties" = {
-      "bluez5.enable-sbc-xq" = true;
-      "bluez5.enable-msbc" = true;
-      "bluez5.enable-hw-volume" = true;
-      "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+    jack.enable = true;
+    wireplumber = {
+      enable = true;
+      package = pkgs.wireplumber;
     };
   };
 }
