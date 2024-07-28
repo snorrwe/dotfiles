22e3efed8d083c3091a976e27c8acc673b003479
Daniel Kiss <littlesnorrboy@gmail.com>
2024-07-29 00:11:23 +0200

    Fix xdg-desktop-portal warning

--- hosts/danilife/config.nix
+++ hosts/danilife/config.nix
@@ -209,6 +209,7 @@
       xdg-desktop-portal-hyprland
       xdg-desktop-portal-gtk
     ];
+    config.common.default = [ "hyprland" ];
   };
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
