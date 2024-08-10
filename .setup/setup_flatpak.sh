4a3da18b89d3ba4ef981bca1c4aff9de6cbb09f9
Daniel Kiss <littlesnorrboy@gmail.com>
2024-08-10 23:52:18 +0200

    Remove unused flatpaks from install script

--- .setup/setup_flatpak.sh
+++ .setup/setup_flatpak.sh
@@ -2,19 +2,12 @@
 
 set -euxo pipefail
 
-flatpak install --noninteractive us.zoom.Zoom com.slack.Slack \
-    com.jgraph.drawio.desktop \
+flatpak install --noninteractive us.zoom.Zoom \
     org.cloudcompare.CloudCompare \
-    org.geeqie.Geeqie \
-    discord \
-    com.bitwarden.desktop  \
-    chrome \
     org.videolan.VLC \
     io.github.martinrotter.rssguard \
     com.google.Chrome \
     blender \
     meshlab \
-    geeqie \
-    md.obsidian.Obsidian \
     krita 
 
--- hosts/snorrwe/commit-message.nix
+++ hosts/snorrwe/commit-message.nix
@@ -1,3 +1,3 @@
 {
-  system.nixos.label = "Fix_hyprland_on_login_____________________________";
+  system.nixos.label = "Remove_unused_flatpaks_from_install_script________";
 }
