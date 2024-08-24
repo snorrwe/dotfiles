d076ebed7ac88dc75589fb464fb606cd8d5ebc7a
Daniel Kiss <littlesnorrboy@gmail.com>
2024-08-24 18:11:23 +0200

    fmt

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
   description = "snorrwe";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"
-    ;
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs:
--- hosts/snorrwe/commit-message.nix
+++ hosts/snorrwe/commit-message.nix
@@ -1,3 +1,3 @@
 {
-  system.nixos.label = "Only_commit_the_flake.lock_on_update______________";
+  system.nixos.label = "fmt_______________________________________________";
 }
