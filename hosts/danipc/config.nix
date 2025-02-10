f6a131af7180afad6dc3f0cb003a52dc983625f1
Daniel Kiss <littlesnorrboy@gmail.com>
2025-02-10 21:19:55 +0100

    Enable npm

--- hosts/danipc/config.nix
+++ hosts/danipc/config.nix
@@ -201,6 +201,7 @@
       loginBackground = true;
     })
   ];
+  programs.npm.enable = true;
   environment.variables.RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
   xdg.portal = {
     enable = true;
