1b08527035303e659c7907d47c73eb65c2e89b7e
Daniel Kiss <littlesnorrboy@gmail.com>
2025-07-10 00:25:38 +0200

    Setup nh

---
+++ modules/nh.nix
@@ -0,0 +1,15 @@
+{
+  pkgs,
+  username,
+  host,
+  inputs,
+  ...
+}:
+{
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = toString inputs.self;
+  };
+}
