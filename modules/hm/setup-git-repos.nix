7084a2bc04dd53443c9e287345d1c9f826cee49f
Daniel Kiss <littlesnorrboy@gmail.com>
2025-07-13 12:18:21 +0200

    Delete existing file when cloning
    e.g. a broken symlink from a previous generation

--- modules/hm/setup-git-repos.nix
+++ modules/hm/setup-git-repos.nix
@@ -2,10 +2,12 @@
 {
   home.activation.setupNvimConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     if [[ ! -d ~/.config/nvim ]]; then
+    	rm -f ~/.config/nvim
         ${pkgs.git}/bin/git clone https://github.com/snorrwe/nvim-config ~/.config/nvim
     fi
     if [[ ! -d ~/.config/tmux/plugins/tpm/ ]]; then
         mkdir -p ~/.config/tmux/plugins
+    	rm -f ~/.config/tmux/plugins/tpm
         ${pkgs.git}/bin/git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
     fi
   '';
