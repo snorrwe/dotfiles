5633d6cee65af7b79917a36604e82f8b1d953e5d
Daniel Kiss <littlesnorrboy@gmail.com>
2025-07-13 23:29:27 +0200

    Remove redundant insecure package

--- hosts/danilife/config.nix
+++ hosts/danilife/config.nix
@@ -162,7 +162,6 @@ in
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.permittedInsecurePackages = [
-    "olm-3.2.16"
   ];
 
   # List packages installed in system profile. To search, run:
