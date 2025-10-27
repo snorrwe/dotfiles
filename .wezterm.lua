c2394348573158fbd803cdc7e2ec69ad6a013355
Daniel Kiss <littlesnorrboy@gmail.com>
2025-10-27 11:28:27 +0100

    Disable wezterm tab bar, show tab navigation on ctrl+shift+o

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config.color_scheme = "Catppuccin Mocha"
@@ -15,5 +15,10 @@ config.enable_wayland = false
 config.warn_about_missing_glyphs = false
 
 config.check_for_updates = false
+
+config.enable_tab_bar = false
+config.keys = {
+	{ key = "O", mods = "SHIFT|CTRL", action = wezterm.action.ShowTabNavigator },
+}
 
 return config
--- modules/commit-message.nix
+++ modules/commit-message.nix
@@ -1,3 +1,3 @@
 {
-  system.nixos.label = "Rename_root_dotfiles______________________________";
+  system.nixos.label = "Disable_wezterm_tab_bar_show_tab_navigation_on_ctr";
 }
